cd8d51a5d4a0bc9512f04a2fc77a7b67f3037bdd
John McGarvey <jmcgarve@bellsouth.net>
2019-02-02 16:41:40 +0000

    create places as per Ramiro

---
+++ places/db/migrate/20190202141433_create_user_places.rb
@@ -0,0 +1,9 @@
+class CreateUserPlaces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_places do |t|
+      t.integer :user_id
+      t.integer :place_id
+      t.timestamps
+    end
+  end
+end
